40d5eac15c78fb9d250b61f7c45bb7fa5cd4cdad
Nikita <river600@rambler.ru>
2024-11-22 15:00:21 +0300

    Add two files for Variables Task hw

---
+++ Lesson_1/VariableNamesTheme.java
@@ -0,0 +1,144 @@
+public class VariableNamesTheme {
+    public static void main(String[] args) {
+        System.out.println("1. Разные переменные");
+        // цифра
+        int digit = 3;
+        // сумма чисел
+        int sumNumbers = 25;
+        // произведение цифр
+        int multiplicationDigits = 78;
+        // максимальное число
+        int maxNumber = 32;
+        // количество десятков
+        int numberDozens = 6;
+        // вес собаки
+        float weightDog = 3.6F;
+        // исходное число
+        int originalNumber = 5892;
+        // процент по вкладу
+        double depositInterest = 15e-2;
+        // (переменная хранит символ) &
+        char ampersand = '&';
+        // код ошибки
+        int codeError = 404;
+        // тип сообщения
+        String typeMessage = "Urgent";
+        // число нулей
+        int numberZeros = 5;
+        // уникальное число
+        int uniqueNumber = 35678991;
+        // случайное число
+        int randomNumber = 885219;
+        // математическое выражение
+        String mathExpression = "Multiplication";
+        // счет в игре
+        String gameScore = " 83 : 72";
+        // максимальная длина
+        int maxLength = 2500;
+        // пункт меню
+        char menuItem = 'a';
+        // стоимость кофе
+        float coffeePrice = 232e-2F;
+        // дата начала
+        String startDate = "19.05.2021";
+        // окончание диапазона
+        int endRange = 27556900;
+        // полное имя работника месяца
+        String fullNameMonthWorker = "John Doe";
+        // заголовок электронной книги
+        String ebookTitle = "War of the Worlds";
+        // размер
+        String size = "XL";
+        // вместимость
+        int capacity = 25869;
+        // счетчик
+        int counter = 0;
+        // путь до файла
+        String filePath = "C:\\\\StartJava\\Lesson_1\\";
+        // количество чисел в каждой строке
+        int amountNumbersInEachString = 23;
+
+        System.out.println("цифра = " + digit);
+        System.out.println("сумма чисел = " + sumNumbers);
+        System.out.println("произведение цифр = " + multiplicationDigits);
+        System.out.println("максимальное число = " + maxNumber);
+        System.out.println("количество десятков = " + numberDozens);
+        System.out.println("вес собаки = " + weightDog);
+        System.out.println("исходное число = " + originalNumber);
+        System.out.println("процент по вкладу = " + depositInterest);
+        System.out.println("переменная хранит символ & = " + ampersand);
+        System.out.println("код ошибки = " + codeError);
+        System.out.println("тип сообщения = " + typeMessage);
+        System.out.println("число нулей = " + numberZeros);
+        System.out.println("уникальное число = " + uniqueNumber);
+        System.out.println("случайное число = " + randomNumber);
+        System.out.println("математическое выражение = " + mathExpression);
+        System.out.println("счет в игре = " + gameScore);
+        System.out.println("максимальная длина = " + maxLength);
+        System.out.println("пункт меню = " + menuItem);
+        System.out.println("стоимость кофе = " + coffeePrice);
+        System.out.println("дата начала = " + startDate);
+        System.out.println("окончание диапазона = " + endRange);
+        System.out.println("полное имя работника месяца = " + fullNameMonthWorker);
+        System.out.println("заголовок электронной книги = " + ebookTitle);
+        System.out.println("размер = " + size);
+        System.out.println("вместимость = " + capacity);
+        System.out.println("счетчик = " + counter);
+        System.out.println("путь до файла = " + filePath);
+        System.out.println("количество чисел в каждой строке = " + amountNumbersInEachString);
+
+        System.out.println("\n2. boolean-переменные");
+        // сотни равны?
+        boolean hasEqualHundreds = false;
+        // компьютер включен?
+        boolean isComputerTurnedOn = false;
+        // есть равные цифры?
+        boolean hasEqualDigits = true;
+        // создано?
+        boolean isCreated = true;
+        // пустое?
+        boolean isEmpty = false;
+        // активное?
+        boolean isActive = true;
+        // новое?
+        boolean isNew = false;
+        // электронная почта действительная?
+        boolean isEmailReal = true;
+        // имеются уникальные строки?
+        boolean hasUniqueStrings = true;
+
+        System.out.println("сотни равны? = " + hasEqualHundreds);
+        System.out.println("компьютер включен? = " + isComputerTurnedOn);
+        System.out.println("есть равные цифры? = " + hasEqualDigits);
+        System.out.println("создано? = " + isCreated);
+        System.out.println("пустое? = " + isEmpty);
+        System.out.println("активное? = " + isActive);
+        System.out.println("новое? = " + isNew);
+        System.out.println("электронная почта действительная? = " + isEmailReal);
+        System.out.println("имеются уникальные строки? = " + hasUniqueStrings);
+
+        System.out.println("\n3. Аббревиатуры");
+        // старый universally unique identifier
+        int oldUuid = 4321;
+        // производитель оперативной памяти
+        String ramManufacturer = "Samsung";
+        // емкость жесткого диска
+        long hddCapacity = 326546897L;
+        // протокол передачи гипертекста
+        int http = 120;
+        // сокращенный uniform resource locator
+        String shortUrl = "www.google.com";
+        // новый идентификатор клиента
+        int newClientId = 4578;
+        // кодировка american standard code for information interchange
+        int ascii = 6;
+
+        System.out.println("старый universally unique identifier = " + oldUuid);
+        System.out.println("производитель оперативной памяти = " + ramManufacturer);
+        System.out.println("емкость жесткого диска = " + hddCapacity);
+        System.out.println("протокол передачи гипертекста = " + http);
+        System.out.println("сокращенный uniform resource locator = " + shortUrl);
+        System.out.println("новый идентификатор клиента = " + newClientId);
+        System.out.println("кодировка american standard code for information interchange = " + ascii);
+    }
+}
---
+++ Lesson_1/VariablesTheme.java
@@ -0,0 +1,149 @@
+import java.math.BigDecimal;
+
+public class VariablesTheme {
+    public static void main(String[] args) {
+        System.out.println("1. Вывод характеристик компьютера");
+        byte cpuCores = 10;
+        short cpuModel = 12500;
+        int ramMemory = 8192;
+        long hddMemory = 536_870_912L;
+        char cpuSerial = 'i';
+        boolean isTurnedOn = false;
+        float price = 500.35F;
+        double cpuFrequency = 3.4;
+
+        System.out.println("Количество ядер процессора: " + cpuCores);
+        System.out.println("Модель процессора: " + cpuModel);
+        System.out.println("Оперативная память: " + ramMemory);
+        System.out.println("Память жесткого диска: " + hddMemory);
+        System.out.println("Серия процессора: " + cpuSerial);
+        System.out.println("Включен ли компьютер: " + isTurnedOn);
+        System.out.println("Цена компьютера: " + price);
+        System.out.println("Частота процессора: " + cpuFrequency);
+
+        System.out.println("\n2. Расчет стоимости товара со скидкой");
+        double penPrice = 105.5;
+        double bookPrice = 235.83;
+        double discount = 0.11;
+        double totalSumWithoutDiscount = penPrice + bookPrice;
+        double discountSum = discount * (penPrice + bookPrice);
+        double discountPrice = totalSumWithoutDiscount - discountSum;
+
+        System.out.println("Стоимость товаров без скидки: " + totalSumWithoutDiscount);
+        System.out.println("Сумма скидки: " + discountSum);
+        System.out.println("Стоимость товаров со скидкой: " + discountPrice);
+
+        System.out.println("\n3. Вывод слова JAVA");
+        System.out.println("   j   a  v     v  a");
+        System.out.println("   j  a a  v   v  a a");
+        System.out.println("j  j aaaaa  v v  aaaaa");
+        System.out.println(" jj a     a  v  a     a");
+
+        System.out.println("\n4. Вывод min и max значений целых числовых типов");
+        byte byteMax = 127;
+        short shortMax = 32767;
+        int intMax = 2_147_483_647;
+        long longMax = Long.MAX_VALUE;
+
+        System.out.println("первоначальное значение: " + byteMax);
+        System.out.println("значение после инкремента на единицу: " + ++byteMax);
+        System.out.println("значение после декремента на единицу: " + --byteMax);
+        System.out.println("первоначальное значение: " + shortMax);
+        System.out.println("значение после инкремента на единицу: " + ++shortMax);
+        System.out.println("значение после декремента на единицу: " + --shortMax);
+        System.out.println("первоначальное значение: " + intMax);
+        System.out.println("значение после инкремента на единицу: " + ++intMax);
+        System.out.println("значение после декремента на единицу: " + --intMax);
+        System.out.println("первоначальное значение: " + longMax);
+        System.out.println("значение после инкремента на единицу: " + ++longMax);
+        System.out.println("значение после декремента на единицу: " + --longMax);
+
+        System.out.println("\n5. Перестановка значений переменных");
+        int a = 2;
+        int b = 5;
+
+        System.out.println("Перестановка с помощью третьей переменной: ");
+        System.out.println("Исходные значения: " + a + " и " + b);
+        int tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println("Новые значения: " + a + " и " + b);
+
+        System.out.println("Перестановка с помощью арифметических операций: ");
+        System.out.println("Исходные значения: " + a + " и " + b);
+        a += b;
+        b = a - b;
+        a -= b;
+        System.out.println("Новые значения: " + a + " и " + b);
+
+        System.out.println("Перестановка  с помощью побитовой операции ^: ");
+        System.out.println("Исходные значения: " + a + " и " + b);
+        a ^= b;
+        b = a ^ b;
+        a ^= b;
+        System.out.println("Новые значения: " + a + " и " + b);
+
+        System.out.println("\n6. Вывод символов и их кодов");
+        char dollar = '$';
+        char asterisk = '*';
+        char atSign = '@';
+        char verticalBar = '|';
+        char tilda = '~';
+
+        System.out.println(dollar + "  " + asterisk + "  " + atSign +
+                "   " + verticalBar + "   " + tilda);
+        System.out.println((int) dollar + " " + (int) asterisk + " " + (int) atSign +
+                " " + (int) verticalBar + " " + (int) tilda);
+
+        System.out.println("\n7. Вывод в консоль ASCII-арт Дюка");
+        char slash = '/';
+        char backslash = '\\';
+        char openedParenthesis = '(';
+        char closedParenthesis = ')';
+        char underline = '_';
+
+        System.out.println("     " + slash + backslash);
+        System.out.println("    " + slash + "  " + backslash);
+        System.out.println("   " + slash + underline +
+                openedParenthesis + " " + closedParenthesis + backslash);
+        System.out.println("  " + slash + "      " + backslash);
+        System.out.println(" " + slash + underline + underline +
+                underline + underline + slash + backslash + underline +
+                underline + backslash);
+
+        System.out.println("\n8. Манипуляции с сотнями, десятками и единицами числа");
+        int number = 123;
+        int hundreds = number / 100;
+        int tens = number % 100 / 10;
+        int ones = number % 10;
+        int sumNumbers = hundreds + tens + ones;
+        int multiplicationNumbers = hundreds * tens * ones;
+
+        System.out.println("Число " + number + " содержит:\n  сотен - " +
+                hundreds + "\n  десятков - " + tens + "\n  единиц - " +
+                ones + "\nСумма его цифр = " + sumNumbers +
+                "\nПроизведение = " + multiplicationNumbers);
+
+        System.out.println("\n9. Перевод секунд в часы, минуты и секунды");
+        int totalNumberSeconds = 86399;
+        int hours = totalNumberSeconds / 3600;
+        int minutes = totalNumberSeconds % 3600 / 60;
+        int seconds = totalNumberSeconds % 3600 % 60;
+
+        System.out.println(hours + ":" + minutes + ":" + seconds);
+
+        System.out.println("\n10. Расчет стоимости товара со скидкой");
+        BigDecimal penPrice2 = new BigDecimal("105.5");
+        BigDecimal bookPrice2 = new BigDecimal("235.83");
+        BigDecimal discount2 = new BigDecimal("0.11");
+        BigDecimal totalSumWithoutDiscount2 = penPrice2.add(bookPrice2).setScale(2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal discountSum2 = discount2.multiply(totalSumWithoutDiscount2).setScale(2, 
+                BigDecimal.ROUND_HALF_UP);
+        BigDecimal discountPrice2 = totalSumWithoutDiscount2.subtract(discountSum2).setScale(2, 
+                BigDecimal.ROUND_HALF_UP);
+
+        System.out.println("Стоимость товаров без скидки: " + totalSumWithoutDiscount2);
+        System.out.println("Сумма скидки: " + discountSum2);
+        System.out.println("Стоимость товаров со скидкой: " + discountPrice2);
+    }
+}
