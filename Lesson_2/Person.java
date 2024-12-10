2f12421c461cf158dc1a393fb8747fd3f3d9bc20
Nikita <river600@rambler.ru>
2024-12-10 19:23:38 +0300

    Person file for Lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "Male";
+    String name = "John";
+    double height = 1.8;
+    double weight = 75.5;
+    int age = 32;
+
+    public void walk() {
+        System.out.println(name + " is walking");
+    }
+
+    public void sit() {
+        System.out.println(name + " is sitting");
+    }
+
+    public void run() {
+        System.out.println(name + " is running");
+    }
+
+    public void say() {
+        System.out.println(name + " is saying");
+    }
+
+    public void learnJava() {
+        System.out.println(name + " is learning Java");
+    }
+}
