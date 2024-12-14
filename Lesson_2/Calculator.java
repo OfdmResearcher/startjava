a59a24f2bc3c5549d3a1e9ab0040518c44d68051
Nikita <river600@rambler.ru>
2024-12-14 15:21:31 +0300

    Bug fixes in Calculator and CalculatorTest classes

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,52 +1,42 @@
 public class Calculator {
-    public void start(int firstNumber, int secondNumber, char sign) {
-        int result = 0;
-        double doubleResult = 0;
+    public void calculate(int firstNumber, int secondNumber, char sign) {
+        double result = 0;
         switch (sign) {
             case '+':
                 result = firstNumber + secondNumber;
-                System.out.println(firstNumber + " " + sign + " " + secondNumber + " = " + result);
                 break;
             case '-':
                 result = firstNumber - secondNumber;
-                System.out.println(firstNumber + " " + sign + " " + secondNumber + " = " + result);
                 break;
             case '*':
                 result = firstNumber * secondNumber;
-                System.out.println(firstNumber + " " + sign + " " + secondNumber + " = " + result);
                 break;
             case '/':
-                if (secondNumber != 0) {
-                    doubleResult = firstNumber / (double) secondNumber;
-                    System.out.println(firstNumber + " " + sign + " " + secondNumber + " = " + doubleResult);
-                } else {
+                if (secondNumber == 0) {
                     System.out.println("Ошибка: деление на ноль запрещено");
+                    break;
                 }
+                result = firstNumber / (double) secondNumber;
                 break;
             case '%':
                 result = firstNumber % secondNumber;
-                System.out.println(firstNumber + " " + sign + " " + secondNumber + " = " + result);
                 break;
             case '^':
                 result = 1;
-                double resultForNegativeExponentiation = 1;
-                if (secondNumber >= 0) {
-                    for (int i = 0; i < secondNumber; i++) {
+                int tempNumber = secondNumber;
+                while (tempNumber != 0) {
+                    if (tempNumber > 0) {
                         result *= firstNumber;
+                        tempNumber--;
+                    } else {
+                        result = result * (1 / (double) firstNumber);
+                        tempNumber++;
                     }
-                    System.out.println(firstNumber + " " + sign + " " + secondNumber + " = " + result);
-                } else {
-                    double tempResult = 1;
-                    for (int i = 0; i < secondNumber * -1; i++) {
-                        tempResult *= firstNumber;
-                        resultForNegativeExponentiation = 1 / tempResult;
-                    }
-                    System.out.println(firstNumber + " " + sign + " " + secondNumber +
-                            " = " + resultForNegativeExponentiation);
                 }
                 break;
             default:
                 System.out.println("Ошибка: операция '" + sign + "' не поддерживается");
         }
+        System.out.println(firstNumber + " " + sign + " " + secondNumber + " = " + result);
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,11 +4,11 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner sc = new Scanner(System.in);
-        userInputAndResult(calculator, sc);
-        userInputAboutContinuingCalculations(calculator, sc);
+        startCalculator(calculator, sc);
+        showUserReply(calculator, sc);
     }
 
-    private static void userInputAndResult(Calculator calculator, Scanner sc) {
+    private static void startCalculator(Calculator calculator, Scanner sc) {
         System.out.print("Введите первое число: ");
         int firstNumber = sc.nextInt();
         System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
@@ -16,20 +16,20 @@ public class CalculatorTest {
         char sign = sc.nextLine().charAt(0);
         System.out.print("Введите второе число: ");
         int secondNumber = sc.nextInt();
-        calculator.start(firstNumber, secondNumber, sign);
+        calculator.calculate(firstNumber, secondNumber, sign);
         sc.nextLine();
     }
 
-    private static void userInputAboutContinuingCalculations(Calculator calculator, Scanner sc) {
+    private static void showUserReply(Calculator calculator, Scanner sc) {
         System.out.print("Хотите продолжить вычисления? [yes/no]: ");
         String userReply = sc.nextLine();
         if (userReply.equals("yes")) {
-            userInputAndResult(calculator, sc);
-            userInputAboutContinuingCalculations(calculator, sc);
+            startCalculator(calculator, sc);
+            showUserReply(calculator, sc);
         } else if (userReply.equals("no")) {
             System.out.println("Конец программы");
         } else {
-            userInputAboutContinuingCalculations(calculator, sc);
+            showUserReply(calculator, sc);
         }
     }
 }
