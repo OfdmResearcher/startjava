3c6c6faa8637307df7b1781d3e5dc50d398ea331
Nikita <river600@rambler.ru>
2026-01-13 11:19:58 +0300

    Add Person class for Lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    char gender = 'м';
+    String name = "Виктор";
+    double height = 1.75;
+    double weight = 81.5;
+    int age = 30;
+
+    void walk() {
+        System.out.println("Идет");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void talk() {
+        System.out.println("Говорит");
+    }
+
+    void studyJava() {
+        System.out.println("Учит Java");
+    }
+}
